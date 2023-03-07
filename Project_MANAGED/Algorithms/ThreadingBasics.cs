066f44bf14d503fca979af7a973c3c3de74cbdbd
mauro_fausto <89610280+MauroFausto@users.noreply.github.com>
2023-03-07 07:51:42 -0300

    Commit inicial
    Commit inicial + primeiro algoritmo

---
+++ Project_MANAGED/Algorithms/ThreadingBasics.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_MANAGED.Algorithms
+{
+    public class ThreadingBasics : IDisposable
+    {
+        private bool disposedValue;
+
+        public ThreadingBasics() { }
+
+        public Task<bool> IsRunning { get; set; }
+
+        public void Run()
+        {
+            Console.WriteLine("Olá mundo assíncrono!\r\n");
+
+            var bgThread = new Thread(() =>
+            {
+                while (true) 
+                {
+                    bool isNetworkUp = NetworkInterface.GetIsNetworkAvailable();
+                    Console.WriteLine($"A conexão de rede está online? Resposta: {isNetworkUp}");
+                    Thread.Sleep( 100 );
+                }
+            });
+
+            bgThread.IsBackground = true;
+            bgThread.Start();
+
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine("Thread principal trabalhando...");
+                Task.Delay(500);
+            }
+
+            Console.WriteLine("Terminou!");
+            Console.ReadKey();
+        }
+
+        #region [  Disposable properties and methods  ]
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~ThreadingBasics()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
---
+++ Project_MANAGED/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Transactions;
+
+namespace Project_MANAGED
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto de demonstração assíncrona iniciou.\r\n");
+
+            using (Algorithms.ThreadingBasics thBasics = new Algorithms.ThreadingBasics())
+            {
+                thBasics.Run();
+            }
+        }
+    }
+}
